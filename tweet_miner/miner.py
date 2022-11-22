dbafd616da7737b56ae301f05932f163df585f90
henokjackson <henokjackson@gmail.com>
2022-11-22 16:03:47 +0530

    Miner Updated

--- tweet_miner/miner.py
+++ tweet_miner/miner.py
@@ -1,11 +1,9 @@
 import tweepy
-
-#Setup authentication
-auth=tweepy.OAuth2BearerHandler("")
-api=tweepy.API(auth)
-
-tweets=api.search_tweets("kerala",tweet_mode="extended")
-
-for tweet in tweets:
-    print(tweet.full_text)
-    print("======================")
+client=tweepy.Client(bearer_token='AAAAAAAAAAAAAAAAAAAAAKlYiAEAAAAAtig4A0Zi16hZ8BNzw17hbBef%2Fs8%3DyCqSNH5GfLa5Kxh6oQiQPxx7gb7Lv2gcrUKIsyjLsT0UyjJGDK')
+query_stmt='#keralapolice lang:en'
+tweets=client.search_recent_tweets(query=query_stmt,tweet_fields=['context_annotations','created_at'],max_results=100)
+for tweet in tweets.data:
+    print(tweet.text)
+    print(tweet.created_at)
+    print("=====================================")
+    #print(tweet.context_annotations)
